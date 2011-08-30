f613f81e48273270ef7ea23423cb91a1b0bafc16
Eric Jonas <jonas@mit.edu>
2011-08-29 18:36:34 -0700

    text generator

---
+++ app/randomtext.py
@@ -0,0 +1,39 @@
+
+text = [
+"""
+Mathematics is the language used for compact description of the order in nature, especially the laws of Physics. This was noted and advocated by Pythagoras,[23] Plato,[24] Galileo,[25] and Newton.
+""", 
+
+"""
+Physics theories use Mathematics[26] to obtain order and provide precise formulas, precise or estimated solutions, quantitative results and predictions. Experiment results in physics are numerical measurements. Technologies based on Mathematics, like computation have made computational physics an active area of research.
+""",
+
+"""
+The distinction between Mathematics and Physics is clear-cut, but not always obvious, especially in Mathematical Physics.
+""",
+"""
+Ontology is a prerequisite for Physics, but not for Mathematics. It means Physics ultimately concerned with descriptions of the real world, while Mathematics is concerned with abstract patterns, even beyond the real world. Thus Physics statements are synthetic, while Math statements are analytic. Mathematics contains hypothesis, while Physics contains theories. Mathematics statements have to be only logically true, while predictions of Physics statements must match observed and experimental data.
+""",
+
+"""
+The distinction is clear-cut, but not always obvious. For example, Mathematical Physics is the application of Mathematics in Physics. Its methods are Mathematical, but its subject is Physical.[27] The problems in this field start with a "Math model of a Physical situation" and a "Math description of a Physical law". Every math statement used for solution has a hard-to-find Physical meaning. The final Mathematical solution has an easier-to-find meaning, because it is what the solver is looking for.
+""",
+
+"""
+Physics is a branch of fundamental science, not practical science.[28] Physics is also called "the fundamental science" because the subject of study of all branches of natural science like Chemistry, Astronomy, Geology and Biology are constrained by laws of physics.[29] For example, Chemistry studies properties, structures, and reactions of the matter, including atoms, molecules, solids, liquids and gases. Properties are bound by laws of physics, like conservation of their energy, mass and charge. Structures are formed because particles exert forces on each other, like electrical force and gravity. And reactions are a change in properties and forces that formed a structure, like the different ways in which atoms form molecules.
+""", 
+"""
+Applied physics is a general term for physics research which is intended for a particular use. An applied physics curriculum usually contains a few classes in an applied discipline, like geology or electrical engineering. It usually differs from engineering in that an applied physicist may not be designing something in particular, but rather is using physics or conducting physics research with the aim of developing new technologies or solving a problem.
+""",
+"""
+The approach is similar to that of applied mathematics. Applied physicists can also be interested in the use of physics for scientific research. For instance, people working on accelerator physics might seek to build better particle detectors for research in theoretical physics.
+""",
+"""
+Physics is used heavily in engineering. For example, Statics, a subfield of mechanics, is used in the building of bridges and other structures. The understanding and use of acoustics results in better concert halls; similarly, the use of optics creates better optical devices. An understanding of physics makes for more realistic flight simulators, video games, and movies, and is often critical in forensic investigations.
+""",
+"""
+With the standard consensus that the laws of physics are universal and do not change with time, physics can be used to study things that would ordinarily be mired in uncertainty. For example, in the study of the origin of the Earth, one can reasonably model Earth's mass, temperature, and rate of rotation, over time. It also allows for simulations in engineering which drastically speed up the development of a new technology.
+""",
+"""
+But there is also considerable interdisciplinarity in the physicist's methods, and so many other important fields are influenced by physics: e.g. presently the fields of econophysics plays an important role, as well as sociophysics.
+"""]
