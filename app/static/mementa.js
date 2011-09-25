042e00d3f437ec7189963b7a3fc5644c26f453ee
Eric Jonas <jonas@mit.edu>
2011-09-24 21:57:47 -0700

    added some widgets

--- app/static/mementa.js
+++ app/static/mementa.js
@@ -1,15 +1,6 @@
 
 $(document).ready(
     function () {
-      
-        $("body").live("click", function (e) {
-                           $('a.menu').parent("li").removeClass("open");
-                       });
-        
-        $("a.menu").live('click', function (e) {
-                             console.log("woooo");
-                             var $li = $(this).parent("li").toggleClass('open');
-                             return false;
-                         });
+
 
 }); 
