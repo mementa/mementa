dc653ceb6a1eccaa6e0597ee9123e2f13a838e15
Eric Jonas <jonas@mit.edu>
2011-09-28 16:04:24 -0700

    Fixed error in wsgi file for app

--- wsgi.py
+++ wsgi.py
@@ -9,4 +9,4 @@ config = json.load(file('/home/dotcloud/environment.json', 'r'))
 
 application.config['DB_URL'] = config['DOTCLOUD_DATA_MONGODB_URL']
 secret_file = config['SECRETS']
-app.config.from_pyfile(secret_file, silent=True)
+application.config.from_pyfile(secret_file, silent=True)
